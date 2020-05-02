60e63426004ea68ebbf7dd0ddec63d61ca38d789
Hugo Voisin <hugov@bam.tech>
2020-05-03 01:13:11 +0200

    feat: can add poll answer

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+let poll = {
+  title: "Sondage",
+  answers: [
+    { id: "11", title: "La Guerre des étoiles" },
+    { id: "12", title: "Le Monde de Nemo" }]
+};
 let answers = { 11: 0, 12: 0 };
 
 const app = express();
@@ -17,12 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 
 app.get('/poll', cors(corsOptions), (req, res) => res.set({
     'Content-Type': 'application/json'
-  }).json({
-    title: "Sondage",
-    answers: [
-      { id: "11", title: "La Guerre des étoiles" },
-      { id: "12", title: "Le Monde de Nemo" }]
-  })
+  }).json(poll)
 );
 
 app.post(
@@ -32,6 +33,18 @@ app.post(
     const answerId = req.body.answerId;    
     answers = { ...answers, [answerId]: answers[answerId] + 1 };
     res.json(answers);
+  }
+);
+
+app.post(
+  '/poll/answer/append',
+  cors(corsOptions),
+  (req, res) => {
+    const id = req.body.id;
+    const title = req.body.title;
+    poll = { ...poll, answers: [ ...poll.answers, { id, title }]};
+    answers = { ...answers, [id]: 0 };
+    res.json(poll);
   }
 );
 
